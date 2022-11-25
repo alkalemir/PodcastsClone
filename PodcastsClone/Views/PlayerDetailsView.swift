27eeefc82409e17010d3fa22dde5b0f1c949da4c
alkalemir <alkalemir@gmail.com>
2022-11-25 20:13:24 +0300

    configured animations and fixed bugs

--- PodcastsClone/Views/PlayerDetailsView.swift
+++ PodcastsClone/Views/PlayerDetailsView.swift
@@ -10,25 +10,38 @@ import AVKit
 
 class PlayerDetailsView: UIView {
     
-    let player: AVPlayer = {
+    var player: AVPlayer = {
         let avPlayer = AVPlayer()
         avPlayer.automaticallyWaitsToMinimizeStalling = false
         return avPlayer
     }()
-    
+
     var episode: Episode! {
         didSet {
             episodeTitle.text = episode.title
             authorLabel.text = episode.author
             episodeImageView.sd_setImage(with: URL(string: episode.imageUrl))
+            episodeImageView.transform = shrunkenTransform
             playEpisode()
             playPauseButton.setImage(UIImage(named: "pause"), for: .normal)
+        }
+    }
+    fileprivate let shrunkenTransform = CGAffineTransform(scaleX: 0.7, y: 0.7)
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        let time = CMTimeMake(value: 1, timescale: 1)
+        let nsValue = NSValue(time: time)
+        player.addBoundaryTimeObserver(forTimes: [nsValue], queue: .main) {
+            self.enlargeImageView()
         }
     }
     
     @IBOutlet weak var playPauseButton: UIButton! {
         didSet {
             playPauseButton.addTarget(self, action: #selector(handlePlayPause), for: .touchUpInside)
+            playPauseButton.imageView?.contentMode = .scaleAspectFit
         }
     }
     
@@ -36,14 +49,21 @@ class PlayerDetailsView: UIView {
         if player.timeControlStatus == .paused {
             player.play()
             playPauseButton.setImage(UIImage(named: "pause"), for: .normal)
+            enlargeImageView()
         } else if player.timeControlStatus == .playing {
             player.pause()
             playPauseButton.setImage(UIImage(named: "play"), for: .normal)
+            collapseImageView()
         }
     }
     
     @IBOutlet weak var authorLabel: UILabel!
-    @IBOutlet weak var episodeImageView: UIImageView!
+    @IBOutlet weak var episodeImageView: UIImageView! {
+        didSet {
+            episodeImageView.layer.cornerRadius = 5
+            episodeImageView.transform = shrunkenTransform
+        }
+    }
     
     @IBOutlet weak var episodeTitle: UILabel! {
         didSet {
@@ -55,9 +75,29 @@ class PlayerDetailsView: UIView {
         removeFromSuperview()
     }
     
+    fileprivate func enlargeImageView() {
+        UIView.animate(withDuration: 0.75, delay: 0, options: .curveEaseOut) {
+            self.episodeImageView.transform = .identity
+        }
+    }
+    
+    fileprivate func collapseImageView() {
+        UIView.animate(withDuration: 0.75, delay: 0, options: .curveEaseIn) {
+            self.episodeImageView.transform = self.shrunkenTransform
+        }
+    }
+    
+    
     fileprivate func playEpisode() {
         guard let url = URL(httpToHttps: episode.mp3url) else { return }
+        print(url)
         player.replaceCurrentItem(with: AVPlayerItem(url: url))
         player.play()
+        
+        do {
+            try AVAudioSession.sharedInstance().setCategory(.playback)
+        } catch let error {
+            print(error.localizedDescription)
+        }
     }
 }
